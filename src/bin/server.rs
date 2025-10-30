9b15c6b2d7de1d205e6e80320bf3aa14022cec79
samosa <moosa.binnaseem09@gmail.com>
2025-10-30 16:21:49 +0500

    Dynamic user defined paths

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,10 +1,8 @@
 use std::net::{SocketAddr, TcpStream, TcpListener};
 use std::io::{BufReader, Write};
-use std::thread;
-use std::time::Duration;
 use std::fs::{File, read};
-
-fn tcp_server(){
+use std::path::{Path, PathBuf};
+fn tcp_server(filePath: String){
     let mut count:i32=0;
     let addrs =[
         SocketAddr::from(([127,0,0,1], 8080)),
@@ -17,12 +15,8 @@ fn tcp_server(){
             Err(e) => println!("Error Receieved {e:?}"),
         }*/
         for stream in listener.incoming(){
-            count +=1;            
-            thread::spawn(||{
             let stream = stream.unwrap();
-            handle_connection(stream);
-            });
-            println!("Thread {}", count);
+            handle_connection(stream,filePath.clone());
         }
     }
     else {
@@ -31,24 +25,26 @@ fn tcp_server(){
 
 }
 
-fn handle_connection(mut stream: TcpStream){
+fn handle_connection(mut stream: TcpStream, filePath: String){
     let buffer = BufReader::new(&stream);
     println!("request successful!!!!!!! 200");
-    let filedata =  openFileAsByte().expect("Couldn't read file");
+    let filedata =  openFileAsByte(filePath).expect("Couldn't read file");
     let ReadData:&[u8] = &filedata.as_slice();
     let sizeOfFile = getMetaData(ReadData);
 
     stream.write(b"Transmitting Filesize\n");
     let mut sizeArr = [0;1];
-    sizeArr = [sizeOfFile];
-    stream.write(&sizeArr);
+    sizeArr = [sizeOfFile];    
+    stream.write(b"File Size = ");
+    stream.write(&sizeArr[0].to_le_bytes());
     stream.write(b"Streaming whole file");
     stream.write(ReadData).expect("Failed to respond!");}
 
 
-fn openFileAsByte()->Result<Vec<u8>, Box<dyn std::error::Error>>{
+fn openFileAsByte(fileName: String)->Result<Vec<u8>, Box<dyn std::error::Error>>{
 
-    let mut fileData = read("src/bin/cpp.cpp").expect("Couldnt read file");
+    let mut fileData = read(fileName).expect("Couldnt read file"); //filename is essentially
+                                                                   //file path
     println!("Read Data {:?}", &fileData);
     println!("Size of file: {}", fileData.len());
     Ok(fileData)
@@ -60,17 +56,40 @@ fn getMetaData(ReadData: &[u8])->u8{
     return size
 }
 
+fn getFilename()->(String, String){
+    loop{
+    println!("Please Enter the File's path that you need to host.");
+    let mut file_string = String::new();
+    let input_path = std::io::stdin().read_line(&mut file_string).unwrap();//saving the value to file_string
+    let trimmed_file_string = file_string.trim();
+    let file_path = PathBuf::from(&trimmed_file_string);
+    
+    if file_path.exists(){
+        //let fileOsString = file_path.as_os_str();
+        if let Some(file) = file_path.file_name(){
+            if let Some(validName) = file.to_str(){
+                return (validName.to_owned(), trimmed_file_string.to_owned());
+            }
+        }
+        
+    }
+    else{
+        println!("File not found, try again!");
+    }}  
+}
 
+   
 fn main() {
+        println!("Welcome to pherrous");
+        let filename: String;
+        let filepath:String;
+        (filename,filepath) = getFilename();
+        println!("Serving file {:?} at 127.0.0.1:8080", filename);
+        tcp_server(filepath);
+      //  println!("{:?}", filepath);
 
-    println!("Opening file cpp");
-    let listen = thread::spawn(|| {
-        tcp_server();
-    });
 
-    listen.join().unwrap();
-    
-    
-}   
+}
+
 
 
