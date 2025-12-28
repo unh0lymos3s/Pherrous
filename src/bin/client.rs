393a2e3d57ec2cef1c4c61511b75f565bd2f800e
samosa <moosa.binnaseem09@gmail.com>
2025-12-29 00:14:41 +0500

    fixed OOM error when recieving filename length

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -7,14 +7,26 @@ fn tcp_client(){
         let mut getStream = match TcpStream::connect("127.0.0.1:8080") {
         
             Ok(mut getStream) => {
-                println!("Recieving file..........");    
+                println!("Recieving file..........");
+                let mut fileNameLengthBuf = [0;1];
                 let mut fileSizeBuf = [0u8; 8];
-                let mut fileNameBuf = vec![0; 9];
-                getStream.read_exact(&mut fileSizeBuf);
+                 
+
+                getStream.read_exact(&mut fileNameLengthBuf);
+                println!("{:?}", fileNameLengthBuf);
+                let fileNameLength = u8::from_be_bytes(fileNameLengthBuf);
+                println!("filename read from be to u64, filenamelength is {}", fileNameLength);
+                let lengthForBuffer = convert_u64_to_usize(fileNameLength);
+                println!("filename converted from ");
+                println!("{}", lengthForBuffer);
+                let mut fileNameBuf = vec![0; lengthForBuffer];
+
                 getStream.read_exact(&mut fileNameBuf);
                 if let Ok(filename) = String::from_utf8(fileNameBuf){
                     println!("Recieving File: {:?}",&filename);
                 }
+                getStream.read_exact(&mut fileSizeBuf);
+                
                 
                // println!("Recieving File Name: {:?}",filename );
                 acceptData(getStream);
@@ -30,6 +42,12 @@ fn tcp_client(){
                 println!("Connection Failed :[");
             }
         };  
+}
+
+fn convert_u64_to_usize(integer: u8)->usize{
+    let int_8: u8 = integer;
+    let int_usize: usize = int_8 as usize;
+    return int_usize
 }
 
 fn acceptData(mut stream: TcpStream)->Result<(), Box<dyn std::error::Error>>{
